fcb4aa295b97f84941c56355bf6b043d9b5d2f76
Mahdi Rahmani <mahdirahmani.cm@gmail.com>
2024-09-28 02:02:30 +0330

    back to main code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,13 +16,7 @@ export default function RootLayout({
 }>) {
    return (
       <html lang='en'>
-         <body
-            style={{
-               fontFamily: `${inter.style.fontFamily}, 'Helvetica Neue', Helvetica, Arial, sans-serif`,
-            }}
-         >
-            {children}
-         </body>
+          <body className={inter.className}>{children}</body>
       </html>
    )
 }
